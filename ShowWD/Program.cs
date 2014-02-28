168105eddeeafcc714b98bf8b975926307561ecb
Paul Saunders <SpaceCuration@gmail.com>
2014-02-28 20:04:10 +1300

    This is the first and probably only commit for this project.

---
+++ ShowWD/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowWD
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Environment.CurrentDirectory);
+
+            if(args.Length > 0)
+            {
+
+                if(args[0] == "-w")
+                    Console.ReadLine();
+
+            }
+
+        }
+
+    }
+
+}
